41dbb929a71190385db5bca4725452aeefcec497
linavon <butterball@Linas-MacBook-Pro.local>
2017-05-12 16:58:55 +0800

    update1

---
+++ JS/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+$(document).on('click', 'a', function(event){
+  // Stop the browser from automatically jumping to the target
+  event.preventDefault();
+
+  // Get the offest location of the target element
+  $('html, body').animate({
+    // Smoothly Move To That Offset
+    scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 1500);
+
+});
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+});
